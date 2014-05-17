e24f6df7d38a2a70cd0365519de1e97ca9891e8c
Toily <escusado@gmail.com>
2014-05-16 21:33:27 -0500

    add pot prototype

--- ArduinoMegaSketch/pix.ino
+++ ArduinoMegaSketch/pix.ino
@@ -1,6 +1,6 @@
 #include <Adafruit_NeoPixel.h>
 
-#define PIN 52
+#define PIN 53
 #define NUM_LEDS (60 * 4)
 
 Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, PIN, NEO_GRB | NEO_KHZ800);
@@ -26,11 +26,35 @@ void loop() {
     if(k==0){
       d=1;
     }
+
     k+=d;
     clearStrip();
-    strip.setPixelColor(k, strip.Color(2, 200, 50));
+    strip.setPixelColor(k,     strip.Color(255, 0, 0));
+    strip.setPixelColor(k-d,   strip.Color(100, 0, 0));
+    strip.setPixelColor(k-d*2, strip.Color(10, 0, 0));
+    strip.setPixelColor(k-d*3,  strip.Color(0, 9, 9));
+    strip.setPixelColor(k-d*4,  strip.Color(0, 9, 9));
+    strip.setPixelColor(k-d*5,  strip.Color(0, 9, 9));
+    strip.setPixelColor(k-d*6,  strip.Color(0, 9, 9));
+    strip.setPixelColor(k-d*7,  strip.Color(0, 9, 9));
+    strip.setPixelColor(k-d*8,  strip.Color(0, 9, 9));
+    strip.setPixelColor(k-d*9,  strip.Color(0, 4, 4));
+    strip.setPixelColor(k-d*10, strip.Color(0, 4, 4));
+    strip.setPixelColor(k-d*11, strip.Color(0, 4, 4));
+    strip.setPixelColor(k-d*12, strip.Color(0, 4, 4));
+    strip.setPixelColor(k-d*13, strip.Color(0, 4, 4));
+    strip.setPixelColor(k-d*14, strip.Color(0, 4, 4));
+    strip.setPixelColor(k-d*15, strip.Color(0, 1, 1));
+    strip.setPixelColor(k-d*16, strip.Color(0, 1, 1));
+    strip.setPixelColor(k-d*17, strip.Color(0, 1, 1));
+    strip.setPixelColor(k-d*18, strip.Color(0, 1, 1));
+    strip.setPixelColor(k-d*19, strip.Color(0, 1, 1));
+    strip.setPixelColor(k-d*20, strip.Color(0, 1, 1));
     strip.show();
     lastMillis = nowMillis;
+
+    // delay(100);
+
 }
 
 void clearStrip(){
